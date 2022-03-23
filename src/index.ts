dbf5ea8c5478dee249b72bd8e1a1551176416b88
vdquijadaenau <vdquijadaenau@gmail.com>
2022-03-23 17:27:19 +1100

    user class created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const user = new User({ name: "victor", age: 12 });
+
+user.set({ name: "jorge", age: 20 });
+console.log(user.get("name"));

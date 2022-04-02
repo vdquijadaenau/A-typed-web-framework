be49ee6d640816415947ccb3d4cea7b1c47e9b13
vdquijadaenau <vdquijadaenau@gmail.com>
2022-04-02 19:24:29 +1100

    user fecth and save

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import { User } from "./models/User";
-
-const user = new User({ name: "victor", age: 12 });
-
-user.set({ name: "jorge", age: 20 });
-console.log(user.get("name"));
+import axios, { AxiosResponse } from "axios";

2570a573f4d742485592187858044e5e8b697c9a
vdquijadaenau <vdquijadaenau@gmail.com>
2022-04-02 21:40:47 +1100

    estructrured classed

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosResponse } from "axios";
+import { Attributes } from "./Atrributes";
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 interface UserProps {
   id?: number;
@@ -6,40 +8,14 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
+const rootUrl = "http://localhost:3000/users";
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach((callback) => callback());
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }

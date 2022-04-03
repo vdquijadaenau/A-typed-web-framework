e78b28a0b8be22ebfe013d42aeaf90c94b554196
vdquijadaenau <vdquijadaenau@gmail.com>
2022-04-03 20:52:49 +1000

    implemented Model Generic and User Class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = User.createUser({ id: 1 });
+
+user.on("change", () => {
+  console.log(user);
+});
+
+user.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,8 @@
+import { AxiosResponse } from "axios";
 import { Attributes } from "./Atrributes";
 import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { Model } from "./Model";
+import { APISync } from "./APISync";
 
 interface UserProps {
   id?: number;
@@ -10,12 +12,12 @@ interface UserProps {
 
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
+export class User extends Model<UserProps> {
+  static createUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new APISync<UserProps>(rootUrl),
+      new Eventing()
+    );
   }
 }

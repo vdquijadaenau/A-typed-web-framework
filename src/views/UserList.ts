c9dd8da60be427f143022ec481887599d6c9a7c0
vdquijadaenau <vdquijadaenau@gmail.com>
2022-04-07 13:35:38 +1000

    UserList added and implementation.

--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -1,7 +1,6 @@
 import { User, UserProps } from "../models/User";
 import { CollectionView } from "./CollectionView";
 import { UserShow } from "./UserShow";
-import { View } from "./View";
 
 export class Userlist extends CollectionView<User, UserProps> {
   renderItem(model: User, itemParent: Element): void {
